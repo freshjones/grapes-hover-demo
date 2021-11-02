80f6393aa96372ab880ff5c87feefe9a43173f2c
William Jones <billy@freshjones.com>
2021-11-02 10:37:16 -0400

    working example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="editor"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'grapesjs/dist/css/grapes.min.css';
+import grapesjs from 'grapesjs';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  mounted () {
+
+    const customComponentType = editor => {
+      editor.DomComponents.addType('custom-site-element', {
+        isComponent: el => el.tagName === 'BLAH',
+        model: {
+          defaults: {
+            editable: false,
+            selectable: false,
+            propagate: ['editable', 'selectable'],
+            tagName: 'blah',
+          }
+        }
+      });
+    };
+
+
+    let editor = grapesjs.init({
+      container: '#editor',
+      fromElement: true,
+      width: 'auto',
+      storageManager: false,
+      plugins: [ customComponentType ],
+    });
+
+
+    editor.BlockManager.add("custom-component",
+      {
+        label: 'Custom Component',
+        tab: "1",
+        category: "Others",
+        type: "default",
+        attributes: {
+          class: 'fa fa-ellipsis-h',
+          title: 'Custom Component'
+        },
+        content: `
+        <blah style="position:relative; display: block;">
+          <h1 data-gjs-hoverable="false">Header test</h1>
+          <p data-gjs-hoverable="false">Paragraph test</p>
+        </blah>`
+    });
+
+    editor.on(`component:hovered`, component => {
+      if (component.attributes.type != 'custom-site-element') {
+          return;
+      }
+      component.append(`<div class="hover-state" style="position:absolute; top:0; left:0; width:100%; height:100%; background:rgba(0,0,0,0.5); z-index:100; display:flex; justify-content: center; align-items:center; backdrop-filter: blur(3px);" data-gjs-hoverable="false"><button data-gjs-hoverable="false" onClick="alert('test')">Edit SWE</button></div>`)
+    })
+
+    editor.on(`component:unhovered`, component => {
+      if (component.attributes.type != 'custom-site-element') {
+          return;
+      }
+      component.get('components').each(child => {
+        let classes = child.getClasses();
+        if(classes.includes('hover-state')) {
+          child.remove()
+        }
+      });
+    })
+
   }
 }
 </script>
